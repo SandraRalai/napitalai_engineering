9474ea22f04f12dd94a063069c8a01dc9e3f0887
Sandra N Ralai <zhandraralai11@gmail.com>
2025-04-22 10:40:26 +1000

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -30,7 +30,7 @@
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['message'], 'Message', 20);
 
   echo $contact->send();
 ?>
